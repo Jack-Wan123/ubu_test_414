1bdbc680585ec1fdab8af82fee3d62c9c94f4942
j.wen <j.wen@supreium.com>
2026-04-15 09:47:50 +0800

    first save

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "和 = " << sum << endl;
+
+    return 0;
+}
